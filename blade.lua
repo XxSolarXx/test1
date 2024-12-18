b80c63272545c0dc5ca864820cba58bb711ab46c
Solar <vanceluke42@gmail.com>
2024-12-18 19:41:23 +0000

    Create blade.lua

---
+++ blade.lua
@@ -0,0 +1,46 @@
+local Window = Rayfield:CreateWindow({
+    Name = "Pluto Auto parry",
+    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+    LoadingTitle = "XxSolarXx has bdokkx in his basement",
+    LoadingSubtitle = "by XxSolarXx",
+    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+ 
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+ 
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "Big Hub"
+    },
+ 
+    Discord = {
+       Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+ 
+    KeySystem = true, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Pluto",
+       Subtitle = "Key System",
+       Note = "Key is Pluto", -- Use this to tell the user how to get a key
+       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"Pluto"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+ })
+
+ local Tab = Window:CreateTab("Auto parry", 4483362458) -- Title, Image
+
+ local Section = Tab:CreateSection("auto parry")
+
+ local Button = Tab:CreateButton({
+    Name = "AutoParry",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/bennettlol/Plutostupidniggascript/refs/heads/main/XxsolarxX",true))()
+    end,
+ })
+
+ local Paragraph = Tab:CreateParagraph({Title = "HELP!", Content = "IM IN XxSolarXx HE IS ABUSING ME HELP HELP"})
